74393f3eff055f8b91df910f13cae5e7917bb808
stunningMary <lyublyukotikov.m.y.r@mail.ru>
2023-12-20 04:21:09 +0500

    first commit

---
+++ app/src/main/java/com/example/notes/notes.java
@@ -0,0 +1,10 @@
+package com.example.notes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class notes {
+    public String name;
+    public String text;
+    public String date;
+}
